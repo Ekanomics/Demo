6a38aefd1fa4905a98c384dcf0fb62031e2fc1df
Ekanomics <165270410+Ekanomics@users.noreply.github.com>
2024-03-30 12:14:01 +0500

    Update Test task calculator.py

--- Test task calculator.py
+++ Test task calculator.py
@@ -21,34 +21,35 @@ def divide (a, b):
 def main():
     print("Welcome, please try my calculator")
 
-    operation = input("Enter operation: ")
+    try:
+        operation = input("Enter operation: ")
 
-    parts = operation.split()
-    if len(parts) != 3:
-        raise ValueError("Incorrect format")
+        parts = operation.split()
+        if len(parts) != 3:
+            raise ValueError("Incorrect format")
 
-    num1 = int(parts[0])
-    operator = parts[1]
-    num2 = int(parts[2])
+        num1 = int(parts[0])
+        operator = parts[1]
+        num2 = int(parts[2])
 
-    if not (1 <= num1 <= 10) or not (1 <= num2 <= 10):
-        raise ValueError("Numbers should be from 1 to 10")
+        if not (1 <= num1 <= 10) or not (1 <= num2 <= 10):
+            raise ValueError("Numbers should be from 1 to 10")
 
-    if operator not in ('+', '-', '*', '/'):
-        raise ValueError("Incorrect operator")
+        if operator not in ('+', '-', '*', '/'):
+            raise ValueError("Incorrect operator")
 
-    if operator == '+':
-        print("Result is:", add(num1, num2))
-    elif operator == '-':
-        print("Result is:", subtract(num1, num2))
-    elif operator == '*':
-        print("Result is:", multiplication(num1, num2))
-    elif operator == '/':
-        print("Result is:", divide(num1, num2))
+        if operator == '+':
+            print("Result is:", add(num1, num2))
+        elif operator == '-':
+            print("Result is:", subtract(num1, num2))
+        elif operator == '*':
+            print("Result is:", multiplication(num1, num2))
+        elif operator == '/':
+            print("Result is:", divide(num1, num2))
+
+    except ValueError as e:
+        print("throws exception:", e)
 
 
 if __name__ == "__main__":
-    try:
-        main()
-    except ValueError as e:
-        print ("Error:", e)
+    main()
